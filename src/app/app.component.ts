b4dce28625d542766f82fda0661a2ec976c2d3c1
Sedky Abou-Shamalah <sedkyaboushamalah@gmail.com>
2019-06-25 13:56:55 -0400

    generated ng-cli

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'frontend';
+}
